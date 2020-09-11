b155c84ae9c280208128102a8fa8cef9574d3ceb
Keyi z <zjenny212@gmail.com>
2020-09-11 13:53:53 -0400

    update

--- Rick and MortyUITests/Rick_and_MortyUITests.swift
+++ Rick and MortyUITests/Rick_and_MortyUITests.swift
@@ -8,27 +8,26 @@
 import XCTest
 
 class Rick_and_MortyUITests: XCTestCase {
-
+    var app: XCUIApplication!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testApp() throws {
+        waitForElement(element: app.staticTexts["Page 1"])
+        XCTAssertTrue(app.navigationBars["Rick and Morty"].exists)
+        app.tables.cells.element(boundBy: 0).tap()
+        waitForElement(element: app.staticTexts["DetailEpisode"])
+        
+        app.tables.cells.element(boundBy: 0).tap()
+        waitForElement(element: app.images.element)
+        XCTAssertTrue(app.staticTexts["cname"].exists)
     }
 
     func testLaunchPerformance() throws {
@@ -38,5 +37,13 @@ class Rick_and_MortyUITests: XCTestCase {
                 XCUIApplication().launch()
             }
         }
+    }
+    
+    func waitForElement(element: XCUIElement, duration: TimeInterval = 10) {
+      let predicate = NSPredicate(format: "exists == true")
+      let _ = expectation(for: predicate, evaluatedWith: element, handler: nil)
+
+      // We use a buffer here to avoid flakiness with Timer on CI
+      waitForExpectations(timeout: duration + 0.5)
     }
 }
